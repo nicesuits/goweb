b3d22a96cf2b41de697078b65e910a46f3971073
Raion314 <raion314@outlook.com>
2019-02-25 19:14:08 -0800

    refactor index route

--- main.go
+++ main.go
@@ -5,15 +5,6 @@ import (
 	"text/template"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
-	files := []string{"templates/layout.html", "templates/navbar.html", "templates/index.html"}
-	templates := template.Must(template.ParseFiles(files...))
-	threads, err := data.Threads()
-	if err == nil {
-		templates.ExecuteTemplate(w, "layout", threads)
-	}
-}
-
 func main() {
 	mux := http.NewServeMux()
 	files := http.FileServer(http.Dir("/public"))
@@ -32,7 +23,7 @@ func main() {
 	mux.HandleFunc("/thread/create", createThread)
 	mux.HandleFunc("/thread/post", postThread)
 	mux.HandleFunc("/thread/read", readThread)
-
+	``
 	server := &http.Server{
 		Addr:    ":8989",
 		Handler: mux,
