9e5a84944472664d9bc0c6551275962201876c5a
Raion314 <raion314@outlook.com>
2019-02-25 19:02:41 -0800

    initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("/public"))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+	mux.HandleFunc("/", index)
+
+	server := &http.Server{
+		Addr:    ":8989",
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
