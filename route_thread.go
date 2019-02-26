8c8a250a422f82a33d877119e494d7497f73d418
Raion314 <raion314@outlook.com>
2019-02-25 20:46:24 -0800

    add logic to thread rotues

--- route_thread.go
+++ route_thread.go
@@ -15,15 +15,65 @@ func newThread(w http.ResponseWriter, r *http.Request) {
 }
 
 func createThread(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("createThread")
+	sess, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		err = request.ParseForm()
+		if err != nil {
+			danger(err, "Cannot parse form")
+		}
+		user, err := sess.User()
+		if err != nil {
+			danger(err, "Cannot get user from session")
+		}
+		topic := r.PostFormValue("topic")
+		if _, err := user.CreateThread(topic); err != nil {
+			danger(err, "Cannot create thread")
+		}
+		http.Redirect(w, r, "/", 302)
+	}
 }
 
 func readThread(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("readThread")
-
+	vals := r.URL.Query()
+	uuid := vals.Get("id")
+	thread, err := data.ThreadByUUID(uuid)
+	if err != nil {
+		errorMessage(w, r, "Cannot read thread")
+	} else {
+		_, err := session(w, r)
+		if err != nil {
+			generateHTML(w, &thread, "layout", "public.navbar", "public.thread")
+		} else {
+			generateHTML(w, &thread, "layout", "private.navbar", "private.thread")
+		}
+	}
 }
 
 func postThread(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("postThread")
-
+	sess, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		err = r.ParseForm()
+		if err != nil {
+			danger(err, "Cannot parse form")
+		}
+		user, err := sess.User()
+		if err != nil {
+			danger(err, "Cannot get user from session")
+		}
+		body := r.PostFormValue("body")
+		uuid := r.PostFormValue("uuid")
+		thread, err := data.ThreadByUUID(uuid)
+		if err != nil {
+			errorMessage(w, r, "Cannot read thread")
+		}
+		if _, err := user.CreatePost(thread, body); err != nil {
+			danger(err, "Cannot create post")
+		}
+		url := fmt.Sprint("/thread/read?id=", uuid)
+		http.Redirect(w, r, url, 302)
+	}
 }
